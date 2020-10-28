56e8c3b2c19a9b979cdb3c5da0982351437449b8
enriqueizel2 <eleaoizel@gmail.com>
2020-10-28 11:23:15 -0400

    feat: criando cobrinha

--- script.js
+++ script.js
@@ -3,10 +3,23 @@ const canvas = document.getElementById("snake");
 //  contexto renderiza o que vai acontecer dentro do canvas
 const context = canvas.getContext("2d");
 const box = 32;
+const snake = [];
+snake[0] = {
+  x: 8 * box,
+  y: 8 * box,
+};
 
 function criarBG() {
   context.fillStyle = "lightgreen";
   context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+function criarCobra() {
+  for (let i = 0; i < snake.length; i++) {
+    context.fillStyle = "green";
+    context.fillRect(snake[i].x, snake[i].y, box, box);
+  }
+}
+
 criarBG();
+criarCobra();
