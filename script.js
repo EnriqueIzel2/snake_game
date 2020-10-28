88bd3d61c05e3d27a9e89cbddf05a8f994764435
enriqueizel2 <eleaoizel@gmail.com>
2020-10-28 11:11:18 -0400

    firt commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const canvas = document.getElementById("snake");
+
+//  contexto renderiza o que vai acontecer dentro do canvas
+const context = canvas.getContext("2d");
+const box = 32;
+
+function criarBG() {
+  context.fillStyle = "lightgreen";
+  context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+criarBG();
